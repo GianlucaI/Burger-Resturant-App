def875e1cc1a89bc62b91e188330c31b61d2d515
Gianluca <gianluca.ianiro@gmail.com>
2018-09-20 12:33:17 -0400

    Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 
 app.use(express.static(__dirname + '/public'));
